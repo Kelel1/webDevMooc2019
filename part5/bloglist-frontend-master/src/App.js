8c45e4420950a669862e594068191848563fd66e
Kern Elder <kelel1@yahoo.com>
2019-09-04 10:37:42 -0400

    add blogs.js methods

--- part5/bloglist-frontend-master/src/App.js
+++ part5/bloglist-frontend-master/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react'
+import loginService from './services/login'
+import blogService from './services/blogs'
+import Blog from './components/Blog'
 
 
 const Form = (props) => { 
@@ -29,25 +32,61 @@ const Form = (props) => {
 
 
 const App = () => {
-  // const [errorMessage, setErrorMessage] = useState(null)
+  const [blogs, setBlogs] = useState([
+    {
+      "title": "09/04/2019",
+      "author": "Kalel",
+      "url": "www.dayof.com",
+      "user": "5d680c41332b07308b5312b1",
+      "likes": 330,
+      "id": "5d6fa3e97f3a6a4ca53c19dd"
+  }
+  ])
+  const [errorMessage, setErrorMessage] = useState(null)
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
-
-  const handleLogin = (event) => {
+  const handleLogin = async (event) => {
     event.preventDefault()
-    console.log('logging in with', username, password)
+    try {
+      const user = await loginService.login({
+        username, password,
+      })
+
+      blogService.setToken(user.token)
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setErrorMessage('Wrong credentials')
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
+    }
   }  
+
+  if (user === null) {
+    return (
+      <div>
+        <h1>
+          Log into Blog-List        
+        </h1>
+        <Form handleLogin = { handleLogin } username = { username } password = { password }
+                setUsername = { setUsername } setPassword = { setPassword }/>
+      </div>
+    )
+  }
 
   return (
     <div>
-      <h1>
-        Log into Blog-List        
-      </h1>
-      <Form handleLogin = { handleLogin } username = { username } password = { password }
-              setUsername = { setUsername } setPassword = { setPassword }/>
+      <h2>Blogs</h2>
+      <p> {user.name} logged in</p>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
