2ef3052abdc3fc26344baef9256ed2150d2d711d
Kern Elder <kelel1@yahoo.com>
2019-09-11 08:30:21 -0400

    add blog css border styles

--- part5/bloglist-frontend-master/src/components/Blog.js
+++ part5/bloglist-frontend-master/src/components/Blog.js
@@ -1,8 +1,23 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingLeft: 5,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+    borderColor: 'gray',
+    borderRadius: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => console.log('clicked')}>
+        {blog.title} {blog.author}
+      </div>
+    </div>
+  
+)}
 
 export default Blog
