c3b4e796471b09a83bc8fb74ad756d53c54e79ab
Kern Elder <kelel1@yahoo.com>
2019-10-14 15:40:33 -0400

    complete unicafe functionality

---
+++ part6/unicafe-redux/src/reducer.js
@@ -0,0 +1,30 @@
+const counterReducer = (state = {good: 0, ok: 0, bad: 0}, action) => {
+  console.log(action)
+  switch (action.type) {
+    case 'GOOD':
+      return state = {
+        ...state,
+        good: state.good + 1
+      }
+    case 'OK':
+      return state = {
+        ...state,
+        ok: state.ok + 1
+      }
+    case 'BAD':
+      return state = {
+        ...state,
+        bad: state.bad + 1
+      }
+    case 'ZERO':
+      return state = {
+        good: 0,
+        bad: 0,
+        ok: 0
+      }
+    default:
+      return state
+  }
+}
+
+export default counterReducer
--- part6/unicafe-redux/src/reducer.test.js
+++ part6/unicafe-redux/src/reducer.test.js
@@ -32,4 +32,65 @@ describe('unicafe reducer', () => {
       bad: 0
     })
   })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('zero is selected', () => {
+    const action = {
+      type: 'ZERO'
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
+  test('current state shown if no action chosen', () => {
+    const action = {
+      
+    }
+    const state = initialState
+
+    deepFreeze(state)
+    const newState = counterReducer(state, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 0
+    })
+  })
+
 })
