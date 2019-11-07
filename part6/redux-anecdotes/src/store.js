734e9b7fde7e04b80cd929b05a1f01c661c1386f
Kern Elder <kelel1@yahoo.com>
2019-11-07 16:33:59 -0500

    refractor store.js to incorporate redux-devtools

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk';
+import thunk                                             from 'redux-thunk'
+import { composeWithDevTools                           } from 'redux-devtools-extension'
 
-import anecdoteReducer from './reducers/anecdoteReducer'
-import searchReducer   from './reducers/filterReducer'
-import notifyReducer from './reducers/notificationReducer'
+import anecdoteReducer                                   from './reducers/anecdoteReducer'
+import searchReducer                                     from './reducers/filterReducer'
+import notifyReducer                                     from './reducers/notificationReducer'
 
 const reducer = combineReducers({
   anecdotes: anecdoteReducer,
@@ -11,6 +12,11 @@ const reducer = combineReducers({
   search:    searchReducer
 })
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const store = createStore(
+  reducer,
+  composeWithDevTools(
+    applyMiddleware(thunk)
+  )
+)
 
 export default store
