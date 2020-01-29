b88589eff2b3fbc1e0de5c59e029d0eab513568d
Kern Elder <kelel1@yahoo.com>
2020-01-29 16:50:16 -0500

    create view for setting birth year

--- part8/library-frontend/src/components/Authors.js
+++ part8/library-frontend/src/components/Authors.js
@@ -1,6 +1,19 @@
 import React, { useState } from 'react'
 
-const Authors = ({ result }) => {
+const Authors = ({ result, props }) => {
+
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const submit = async (e) => {
+    e.preventDefault()
+
+    setName('')
+    setBorn('')
+  }
+
+  
+  
   if (result.loading) {
     return <div>loading...</div>
   }
@@ -29,6 +42,26 @@ const Authors = ({ result }) => {
           )}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <div>
+        <form onSubmit={submit}>
+          <div>
+            name
+            <input
+              value={name}
+              onChange={({ target }) => setName(target.value)}
+            />
+          </div>
+          <div>
+            born
+            <input
+              value={born}
+              onChange={({ target }) => setBorn(Number(target.value))}
+            />
+          </div>
+          <button type="submit">update author</button>
+        </form>
+      </div>
 
     </div>
   )
